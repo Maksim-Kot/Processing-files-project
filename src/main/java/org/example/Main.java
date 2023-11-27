c606a16f76c6d866cd03cd392141df244f7082aa
Maksim Kot <kotmaksimarturovich@gmail.com>
2023-11-27 15:32:20 +0300

    Added TXTReadWrite class in end-to-end_project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import static org.example.JsonReadWrite.readFromJSONFile;
 import static org.example.JsonReadWrite.writeToJSONFile;
+import static org.example.TXTReadWrite.readFromTXTFile;
+import static org.example.TXTReadWrite.writeToTXTFile;
 import static org.example.XMLReadWrite.readFromXML;
 import static org.example.XMLReadWrite.writeToXML;
 
@@ -44,5 +46,19 @@ public class Main {
         {
             System.out.println("Equation: " + equation.getEquation() + ", Variables: " + equation.getVariables() + ", Result: " + equation.getResult());
         }
+
+
+        // Добавление новых уравнений в коллекцию (пример)
+        //equations.add(new MathEquation("2x + 5 = 10", "x", 5));
+        //equations.add(new MathEquation("3y - 7 = 14", "y", 7));
+        System.out.println();
+        writeToTXTFile(equations, "equations.txt");
+
+        List<MathEquation> equations3 = readFromTXTFile("equations.txt");
+        for (MathEquation equation : equations3)
+        {
+            System.out.println("Equation: " + equation.getEquation() + ", Variables: " + equation.getVariables() + ", Result: " + equation.getResult());
+        }
+
     }
 }
---
+++ src/main/java/org/example/TXTReadWrite.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TXTReadWrite {
+    // Метод для чтения коллекции MathEquation из файла
+    public static List<MathEquation> readFromTXTFile(String filename) {
+        List<MathEquation> equations = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split("; ");
+                if (parts.length == 3) {
+                    String equation = parts[0];
+                    String variables = parts[1];
+                    int result = Integer.parseInt(parts[2]);
+                    equations.add(new MathEquation(equation, variables, result));
+                }
+            }
+        } catch (IOException | NumberFormatException e) {
+            e.printStackTrace();
+        }
+        return equations;
+    }
+
+    // Метод для записи коллекции MathEquation в файл
+    public static void writeToTXTFile(List<MathEquation> equations, String filename) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filename))) {
+            for (MathEquation equation : equations) {
+                bw.write(equation.getEquation() + "; " + equation.getVariables() + "; " + equation.getResult());
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
