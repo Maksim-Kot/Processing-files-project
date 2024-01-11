c76fc9b68ff0f08bdcd6cea6d2869c5f84f1d53c
Maksim Kot <kotmaksimarturovich@gmail.com>
2024-01-12 01:13:23 +0300

    Added FolderCreator in end-to-end_project

---
+++ src/main/java/AdditionalClasses/FolderCreator.java
@@ -0,0 +1,23 @@
+package AdditionalClasses;
+
+import java.io.File;
+
+public class FolderCreator {
+    public static boolean createFolder(String folderPath) {
+        File folder = new File(folderPath);
+
+        if (!folder.exists()) {
+            if (folder.mkdirs()) {
+                //System.out.println("Folder successfully created.");
+                return true;
+            } else {
+                System.out.println("Failed to create the folder.");
+                return false;
+            }
+        } else {
+            System.out.println("The folder already exists.");
+            return false;
+        }
+    }
+}
+
